1913b486e8a289f97f8fafd740046ceeadd1e46c
Zander Noriega <zander.noriega@gmail.com>
2022-05-13 09:32:53 -0300

    commit: feat: generics and readonly for primitive constructions.

--- src/music/primitives.ts
+++ src/music/primitives.ts
@@ -2,7 +2,9 @@
 
 type Length = number;
 
-type Note<T> = T & { length: Length, dotted?: boolean }
+type Note<T> = { length: Length, dotted?: boolean | undefined } & T
+
+export const note = (length: Length) => <T>(x: T): Note<T> => ({ length, ...x })
 
 export const dotted = <T>(x: Note<T>): Note<T> => ({ ...x, dotted: true });
 
@@ -21,15 +23,15 @@ type Guitar = { str: GuitarString; fret: Fret; }
 type GuitarNote = Note<Guitar>
 
 export const guitarNote = (length: Length) => (fret: Fret, str: GuitarString = 7): GuitarNote =>
-  ({ str, fret, length })
+  note(length)({ str, fret })
 
 export const half:      (x: Fret) =>  GuitarNote  = guitarNote(1/2);
 export const quarter:   (x: Fret) =>  GuitarNote  = guitarNote(1/4);
 export const eighth:    (x: Fret) =>  GuitarNote  = guitarNote(1/8);
 export const sixteenth: (x: Fret) =>  GuitarNote  = guitarNote(1/16);
 
-export const eighths    = (xs: Fret[]): Group<Guitar> => group(xs.map(eighth));
-export const sixteenths = (xs: Fret[]): Group<Guitar> => group(xs.map(sixteenth));
+export const eighths    = (xs: readonly Fret[]): Group<Guitar> => group(xs.map(eighth));
+export const sixteenths = (xs: readonly Fret[]): Group<Guitar> => group(xs.map(sixteenth));
 
 export const silence = (x: GuitarNote): GuitarNote => ({ ...x, fret: '-' });
 
