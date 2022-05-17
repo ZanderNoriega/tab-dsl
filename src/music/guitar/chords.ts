1e2ddfe811d99ff5f8ff05ab65dd4a1f3e4208cd
Zander Noriega <zander.noriega@gmail.com>
2022-05-17 15:44:22 -0300

    refactor: migrate chord to typescript.

--- src/music/guitar/chords.ts
+++ src/music/guitar/chords.ts
@@ -8,24 +8,26 @@ import {
   twoOrMore,
 } from '../../std/checked-array';
 
-type ChordStringArray =
-  | (NoDupesArray<GuitarNote> & TwoOrMoreArray<GuitarNote>)
-  | null;
+type ChordStringArray = NoDupesArray<GuitarNote> & TwoOrMoreArray<GuitarNote>;
 
 export const chord = (xs: GuitarNote[]): GuitarNote[] => {
-  let checkedStrs: ChordStringArray = twoOrMore(
+  let checkedStrs: ChordStringArray | null = twoOrMore(
     noDupes((x: GuitarNote) => x.str)(checked(xs))
   );
   if (checkedStrs == null) {
     return xs;
   } else {
-    const notes: GuitarNote[] = checkedStrs.values;
-    const length = notes.length;
-    const marked = notes.map((x: GuitarNote, i: number) => {
-      const inChord: ChordState =
-        i == 0 ? 'start' : i == length - 1 ? 'end' : 'in';
-      return { ...x, inChord };
-    });
-    return marked;
+    return buildChord(checkedStrs);
   }
+};
+
+export const buildChord = (xs: ChordStringArray): GuitarNote[] => {
+  const notes: GuitarNote[] = xs.values;
+  const length = notes.length;
+  const marked = notes.map((x: GuitarNote, i: number) => {
+    const inChord: ChordState =
+      i == 0 ? 'start' : i == length - 1 ? 'end' : 'in';
+    return { ...x, inChord };
+  });
+  return marked;
 };
